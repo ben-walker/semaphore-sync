3ff68445e2340a8e2a97cb897414a0de411fc797
Ben Walker <bwalke05@gmail.com>
2019-03-30 12:56:17 -0400

    Code formatting

--- ibadcnt.c
+++ ibadcnt.c
@@ -4,23 +4,20 @@
 #include <stdlib.h>
 
 int NITER = 1000000;
-
 int cnt = 0;
 
-void * Count(void * a)
-{
-    int i, tmp;
-    for(i = 0; i < NITER; i++)
-    {
-        tmp = cnt;      /* copy the global cnt locally */
-        tmp = tmp+1;    /* increment the local copy */
-        cnt = tmp;      /* store the local value into the global cnt */ 
-    }
+void *Count(void *a) {
+   int tmp;
+   for (int i = 0; i < NITER; i += 1) {
+      tmp = cnt; // copy the global cnt locally
+      tmp += 1; // increment the local copy
+      cnt = tmp; // store the local value into the global cnt
+   }
+   return NULL;
 }
 
-int main(int argc, char * argv[])
-{
-	pthread_t tid1, tid2;
+int main(int argc, char *argv[]) {
+   pthread_t tid1, tid2;
 
 	// validate arguments
 	// implementation
@@ -39,32 +36,28 @@ int main(int argc, char * argv[])
 // End of code section 
 
 	// creating Thread 1
-	if(pthread_create(&tid1, NULL, Count, NULL))
-	{
+	if (pthread_create(&tid1, NULL, Count, NULL)) {
 		printf("\n ERROR creating thread 1");
 		exit(1);
 	}
 
 	// creating Thread 2
-	if(pthread_create(&tid2, NULL, Count, NULL))
-	{
+	if (pthread_create(&tid2, NULL, Count, NULL)) {
 		printf("\n ERROR creating thread 2");
 		exit(1);
 	}
 
-	if(pthread_join(tid1, NULL))	/* wait for the thread 1 to finish */
-	{
+	if(pthread_join(tid1, NULL)) { // wait for the thread 1 to finish
 		printf("\n ERROR joining thread");
 		exit(1);
 	}
 
-	if(pthread_join(tid2, NULL))        /* wait for the thread 2 to finish */
-	{
+	if(pthread_join(tid2, NULL)) { // wait for the thread 2 to finish
 		printf("\n ERROR joining thread");
 		exit(1);
 	}
 
-        // Display the value of count cnt
+   // Display the value of count cnt
 // PLEASE DO NOT remove or modify the following code
 	printf("\nCounter is [%d]\n", cnt);
 	if (cnt == 2 * NITER) 
@@ -75,5 +68,3 @@ int main(int argc, char * argv[])
 
 	pthread_exit(NULL);
 }
-
-
